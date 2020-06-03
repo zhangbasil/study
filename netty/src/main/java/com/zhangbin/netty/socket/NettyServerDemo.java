045689a36b69fcdc7b433c87822d1534894b6c96
zhangbin <zhangbin@maxima-cards.com>
2020-06-03 15:17:45 +0800

    Add netty core

--- netty/src/main/java/com/zhangbin/netty/socket/NettyServerDemo.java
+++ netty/src/main/java/com/zhangbin/netty/socket/NettyServerDemo.java
@@ -7,11 +7,23 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
 
 import java.net.SocketAddress;
+import java.util.Date;
 
 /**
  * @author <a href="mailto:hbsy_zhb@163.com">zhangbin</a>
+ * NIO
+ *
+ * 线程模型 reactor
+ * idle监测（dubbo 心跳）
+ * byteBuf  （内存池）
+ * io多路复用 select pool epoll kqueue（mac）
+ * 直接内存（堆外层） 零拷贝
+ * 粘包半包（固定长度，分隔符，固定长度存字段）
+ *
  */
 public class NettyServerDemo {
 
@@ -28,6 +40,7 @@ public class NettyServerDemo {
                     @Override
                     protected void initChannel(SocketChannel ch) throws Exception {
                         ch.pipeline().addLast(new StringDecoder())
+                                .addLast(new IdleStateHandler(0, 0, 3))
                                 .addLast(new StringEncoder())
                                 .addLast(new OperationHandler());
                     }
@@ -65,6 +78,11 @@ public class NettyServerDemo {
         @Override
         public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
             super.userEventTriggered(ctx, evt);
+            if (evt instanceof IdleStateEvent) {
+                System.out.println("new Date() = " + new Date());
+//                ctx.channel().close();
+                ctx.channel().writeAndFlush("heartBeat");
+            }
         }
 
         @Override
