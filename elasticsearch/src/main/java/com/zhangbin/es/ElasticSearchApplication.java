2c51345dd392aab6d4b59e45fa1851cc108aae58
zhangbin <zhangbin@maxima-cards.com>
2020-06-04 10:21:36 +0800

    Add es module

---
+++ elasticsearch/src/main/java/com/zhangbin/es/ElasticSearchApplication.java
@@ -0,0 +1,14 @@
+package com.zhangbin.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:hbsy_zhb@163.com">zhangbin</a>
+ */
+@SpringBootApplication
+public class ElasticSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticSearchApplication.class, args);
+    }
+}
