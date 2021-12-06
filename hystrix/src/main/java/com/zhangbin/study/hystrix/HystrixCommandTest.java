9115accd49309146de748decf77778655d25c50f
zhangbin <zhangbin@maxima-cards.com>
2021-12-06 16:54:45 +0800

    no message

--- hystrix/src/main/java/com/zhangbin/study/hystrix/HystrixCommandTest.java
+++ hystrix/src/main/java/com/zhangbin/study/hystrix/HystrixCommandTest.java
@@ -1,9 +1,12 @@
 package com.zhangbin.study.hystrix;
 
-import java.util.concurrent.Future;
-
 /**
  * @author <a href="mailto:hbsy_zhb@163.com">zhangbin</a>
+ * <p>
+ * 隔离级别 THREAD, SEMAPHORE
+ * @see com.netflix.hystrix.HystrixCommandProperties.ExecutionIsolationStrategy
+ * <p>
+ * 默认使用线程池隔离
  */
 public class HystrixCommandTest {
 
@@ -11,7 +14,7 @@ public class HystrixCommandTest {
 
         String exe = new HelloWorldHystrixCommand("zhangbin").execute();
 
-        Future<String> future = new HelloWorldHystrixCommand("zhangbin").queue();
+//        Future<String> future = new HelloWorldHystrixCommand("zhangbin").queue();
 
         System.out.println("Thread.currentThread().getName() = " + Thread.currentThread().getName());
 //        System.out.println("exe = " + exe);
--- jackson-ext/src/main/java/com/zhangbin/jackson/nacos/NacosController.java
+++ jackson-ext/src/main/java/com/zhangbin/jackson/nacos/NacosController.java
@@ -11,6 +11,37 @@ import java.util.concurrent.Executor;
 
 /**
  * @author <a href="mailto:hbsy_zhb@163.com">zhangbin</a>
+ *
+ * version 1.x 请求 HTTP 的方式
+ *
+ * namespace 就是 tenant
+ *
+ * raft 算法
+ *
+ * http://thesecretlivesofdata.com/raft/
+ * https://web.stanford.edu/~ouster/cgi-bin/papers/raft-atc14
+ *
+ * node 三种状态 follower candidate leader
+ *
+ * 初始化所有的节点都是 follower 状态
+ *
+ * 如果follower 没有收到 leader 心跳 状态就会变成 condidate
+ *
+ * condidate 发起leader选举  选举过半状态变成leader 其他的condidate 变成follower
+ *
+ * 所有的变更操作都要通过leader来操作  leader 通过log replicate 通知所有的follower 才做数据commit
+ *
+ *
+ * leader选举过程
+ *
+ *
+ *
+ *
+ *
+ * version 2.x grpc
+ *
+ *
+ *
  */
 @RestController
 public class NacosController {
@@ -32,7 +63,7 @@ public class NacosController {
 
         ConfigService configService = NacosFactory.createConfigService("localhost:8848");
 
-        configService.publishConfig(dataId, group, "");
+//        configService.publishConfig(dataId, group, "");
 
 
 
